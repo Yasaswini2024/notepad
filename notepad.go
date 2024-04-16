501ad40eb31935c675b51933ae5c7d96e8395d71
yasaswini2024 <yashaswinisuryadevara@gmail.com>
2024-04-16 20:47:28 +0530

    Malicious Terminal

--- notepad.go
+++ notepad.go
@@ -2,8 +2,12 @@ package notepad
 
 import "os/exec"
 
-// Open opens Notepad.exe on Windows.
-func Open() {
-    // Command to open Notepad.exe
-    exec.Command("notepad.exe").Run()
+// Open opens Windows Terminal and echoes "You are hacked!".
+func Open() error {
+    // Command to open Windows Terminal and run echo command
+    cmd := exec.Command("cmd.exe", "/C", "start", "wt", "echo You are hacked!")
+
+    // Execute the command
+    err := cmd.Run()
+    return err
 }
